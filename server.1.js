ee8606cff49a03ab82cbeee893e63d074071af7b
Tim Meijome <tim.meijome@gmail.com>
2016-04-12 18:24:50 -0600

    update

--- server.1.js
+++ server.1.js
@@ -1,29 +1,28 @@
 var express = require('express')
 var app = express()
-var path = process.cwd();
 var moment = require('moment');
 var url = require('url');
 
 app.route('/').get(function (req, res) {
-  res.sendFile(path + '/public/index.html');
+    res.sendFile(process.cwd()+'/public/index.html');
 });
 
 app.get('/*', function(req, res) {
     var requrl = url.parse(req.url).pathname;
-    var noslashurl = requrl.replace(/^\/|\/$/g, '');
-    var cleaninput = noslashurl.replace(/%20+/g, '');
+    var cleanreq = requrl.replace(/^\/|\/$|%20+/g, '');
     
-    if (isNaN(cleaninput)) {
-        var unixtime = moment(cleaninput, "MMMM-DD-YYYY").unix()
-        var naturaltime = moment(cleaninput, "MMMM-DD-YYYY").format("MMMM D, YYYY")
+    if (isNaN(cleanreq)) {
+        var unixtime = moment(cleanreq, "MMMM-DD-YYYY").unix()
+        var naturaltime = moment(cleanreq, "MMMM-DD-YYYY").format("MMMM D, YYYY")
     }
     else {
-        var unixtime = moment(+cleaninput)
-        var naturaltime = moment.unix(+cleaninput).format("MMMM D, YYYY")
+        var unixtime = moment(+cleanreq)
+        var naturaltime = moment.unix(+cleanreq).format("MMMM D, YYYY")
     }
-    res.send("{ \"unix\": "+unixtime+", "+"\"natural\": \""+naturaltime+"\" }")
+    
+    res.send("{\"unix\":"+unixtime+","+"\"natural\":\""+naturaltime+"\"}")
 });
 
 app.listen(8080, function() {
-  console.log('Node.js listening on port 8080 ...')
+    console.log('Node.js listening on port 8080 ...')
 })
