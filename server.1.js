75b78241ff36f34ac0c468934f862d5bdcac799f
Tim Meijome <tim.meijome@gmail.com>
2016-04-12 21:02:37 -0600

    update

--- server.1.js
+++ server.1.js
@@ -4,25 +4,29 @@ var moment = require('moment');
 var url = require('url');
 
 app.route('/').get(function (req, res) {
-    res.sendFile(process.cwd()+'/public/index.html');
+	res.sendFile(process.cwd()+'/public/index.html');
 });
 
 app.get('/*', function(req, res) {
     var requrl = url.parse(req.url).pathname;
     var cleanreq = requrl.replace(/^\/|\/$|%20+/g, '');
     
-    if (isNaN(cleanreq)) {
-        var unixtime = moment(cleanreq, "MMMM-DD-YYYY").unix()
-        var naturaltime = moment(cleanreq, "MMMM-DD-YYYY").format("MMMM D, YYYY")
-    }
+    if (moment(+cleanreq).isValid()|moment(cleanreq).isValid()){
+        if (isNaN(cleanreq)) {
+            var unixtime = moment(cleanreq.toString(), "MMMM-DD-YYYY").unix()
+            var naturaltime = moment(cleanreq.toString(), "MMMM-DD-YYYY").format("MMMM D, YYYY")
+        }
+        else {
+            var unixtime = moment(+cleanreq)
+            var naturaltime = moment.unix(+cleanreq).format("MMMM D, YYYY")
+        }}
     else {
-        var unixtime = moment(+cleanreq)
-        var naturaltime = moment.unix(+cleanreq).format("MMMM D, YYYY")
+        var unixtime = "null"
+        var naturaltime = "null"
     }
-    
     res.send("{\"unix\":"+unixtime+","+"\"natural\":\""+naturaltime+"\"}")
 });
 
 app.listen(8080, function() {
-    console.log('Node.js listening on port 8080 ...')
+	console.log('Node.js listening on port 8080 ...')
 })
