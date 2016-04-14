e10b26b99e613a059f8d5ceccb8f4fb44ee00b0d
Tim Meijome <reeebot@gmail.com>
2016-04-13 19:14:10 -0600

    Update server.js

--- server.js
+++ server.js
@@ -34,6 +34,6 @@ app.get('/*', function(req, res) {
     res.send(JSON.stringify(jsonstring))
 });
 
-app.listen(8080, function() {
+app.listen(80, function() {
 	console.log('Node.js listening on port 8080 ...')
 })
